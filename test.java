fee3b2870186978dde5c7a7812efd6304f4736dc
Collyz <m.mowla03151@gmail.com>
2021-02-19 22:30:02 -0500

    Create test.java

---
+++ test.java
@@ -0,0 +1,8 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class test {
+    public static void main (String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
