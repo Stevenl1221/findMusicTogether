ee7152e741f7ec2f355e13ffe55cec6aa8d6fb55
Steven Lee <steven.huu.ly@gmail.com>
2021-02-20 21:31:47 -0500

    letsgoo

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -48,19 +48,50 @@ return (
 </nav>    
 
 <div class="icon-bar">
-  <a href="#" class="facebook"><i class="fa fa-facebook"></i></a> 
+
+  <a href="#" class="facebook"><i class="fa fa-facebook"></i>
+  <img src="https://bullerockgc.com/wp-content/uploads/2018/05/facebook-logo-png-white-i6.png" alt="instagram" width="45" height="45"></img> </a> 
+  
   <a href="#" class="twitter"><i class="fa fa-twitter"></i></a> 
-  <a href="#" class="google"><i class="fa fa-google"></i></a> 
+
+  <a href="#" class="instagram"><i class="fa fa-instagram"></i>
+  <img src="https://facebookbrand.com/wp-content/uploads/2019/10/Copy-of-instagram.svg" alt="instagram" width="25" height="25"></img> </a> 
   <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
   <a href="#" class="youtube"><i class="fa fa-youtube"></i></a> 
 </div>
 
 
+<div>
 <a href="https://www.google.com/" class="btn btn-primary btn-lg centered">Better Button</a>
+</div>
   
 <div class="progress"></div> 
 
+<div>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="social-share.css" />
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
 </div>
 
-)
+<ul class="ct-socials">
+   <li>
+      <a href="/" target="_blank"><i class="fa fa-twitter"></i></a>
+   </li>
+   <li>
+      <a href="/" target="_blank"><i class="fa fa-facebook"></i></a>
+   </li>
+   <li>
+      <a href="/" target="_blank"><i class="fa fa-pinterest"></i></a>
+   </li>
+   <li>
+      <a href="/" target="_blank"><i class="fa fa-linkedin"></i></a>
+   </li>
+   <li>
+      <a href="/" target="_blank"><i class="fa fa-flickr"></i></a>
+   </li>
+</ul>
+
+</div>
+);
 }
