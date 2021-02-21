45a1750bf4b1ce608791895c6845dfbc404217fc
PlanACat <wahedulmahedi@gmail.com>
2021-02-21 02:00:51 -0500

    updated change
    For momo my love

--- findmusictogether/findMusicTogether/src/components/test.jsx
+++ findmusictogether/findMusicTogether/src/components/test.jsx
@@ -5,18 +5,6 @@ return (
 
 <div>
 
-<nav id="siteNav" class="navbar navbar-default navbar-fixed-top affix-top" role="navigation">
-  <div class="container">
-    <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    </div>
-  </div>
-</nav>
 
 
 
